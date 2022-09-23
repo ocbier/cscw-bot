2670ae638a99cbf6d7225cc18ed434e27a11b4b9
Oloff Biermann <51513555+ocbier@users.noreply.github.com>
2022-09-23 03:41:18 -0700

    Implemented playlist feature

--- bot.py
+++ bot.py
@@ -25,16 +25,10 @@ class Bot:
 
 
     # Sends a message to a channel with the matching channel name.
-    async def send_message_by_name(self, channel_name, channel_number, message):
+    async def send_message_by_name(self, channel_name, message):
 
-        str_channel_number = str(channel_number)
-        if channel_number < 10:
-            str_channel_number = '0' + str_channel_number
-
-        f_name = channel_name.strip().lower().replace(' ', '-').replace('&', '-')
-        f_name = str_channel_number + '-' + f_name
-        print('Sending message to channel with name ' + f_name)
-        channel_obj = discord.utils.get(self.client.get_all_channels(), name=f_name, type=discord.ChannelType.text)
+       
+        channel_obj = discord.utils.get(self.client.get_all_channels(), name=channel_name, type=discord.ChannelType.text)
 
         if channel_obj is None:
             raise ChannelNotFoundException('Error sending message to channel. Could not get the channel with name ' + channel_name)
@@ -43,6 +37,16 @@ class Bot:
 
         
 
+    @staticmethod
+    def get_valid_name(channel_name, channel_number):
+        str_channel_number = str(channel_number)
+        if channel_number < 10:
+            str_channel_number = '0' + str_channel_number
+
+        f_name = channel_name.strip().lower().replace(' ', '-').replace('&', 'and')
+        f_name = str_channel_number + '-' + f_name
+
+        return f_name
 
 
 
--- cscw-tv.py
+++ cscw-tv.py
@@ -9,58 +9,79 @@ from player import VLCPlayer
 from bot import Bot
 from dotenv import load_dotenv
 
-media_path = 'videos'
-sessions_path = 'scheduling'
 
 
-class PaperVideo:
-    def __init__(self, title, id, session, session_number, talk_number = 0, video_path = '', presenter = ''):
-        self.title = title
-        self.id = id
-        self.session = session
+class SessionVideo:
+    def __init__(self, session_number, video_path, paper = None):
         self.session_number = session_number
         self.video_path = video_path
-        self.presenter = presenter
+        self.paper = paper
+
+    def is_paper(self):
+        return self.paper is not None
+
+
+class Paper :
+    def __init__(self, title, id, talk_number): 
+        self.title = title
+        self.id = id
         self.talk_number = talk_number
-        
+
+    @staticmethod
+    def get_paper (papers, cycle, id):
+        for i, paper in papers.iterrows():
+            if paper["cycle"] == cycle and paper["paper_id"] == id:
+                return Paper(title = paper["title"], id = paper["paper_id"], talk_number = paper["talk_number"])
+
 
 
 class CSCWManager:
-    def __init__(self, bot_token, tv_channel_id):
+    def __init__(self, bot_token, tv_channel_id, playlist_file, papers_file, media_path):
         self.bot = Bot(bot_token) # Create the bot
         self.player = VLCPlayer() # Create the player
         self.tv_channel_id = tv_channel_id
+        self.playlist_file = playlist_file
+        self.papers_file = papers_file
+        self.media_path = media_path
+
+        self.current_session_name = 'Idle'
+        self.current_session_number = -1
     
     def sort_papers(self, paper):
         return paper.talk_number
 
-    def create_session_message(self, papers):
-        message = 'The session ' + papers[0].session + ' is about to start!\n\nThe following papers will be presented:'
+    def create_session_message(self, session_videos, session_name):
+        message = 'The session ' + session_name + ' is about to start!\n\nThe following papers will be presented:'
 
         # Add titles to the announcement message
         count = 0 
-        for count, paper in enumerate(papers, start=1):
-            message = message + '\n' + str(count) + '. ' + paper.title
+        for count, video in enumerate(session_videos, start=1):
+            if video.is_paper():
+                message = message + '\n' + str(count) + '. ' + video.paper.title
 
         return message
 
 
-    # Sends messages to channel corresponding to the session for each paper and then play each paper presentation
-    async def broadcast_session(self, papers):
 
-        sorted_papers = sorted(papers, key=self.sort_papers)
 
-        for paper in sorted_papers:
-            message = 'The video presentation for ' + paper.title + ' will be starting now!'
+
+    # Sends messages to session channel (before playback) for each video that is a paper presenation and then plays all session videos.
+    async def broadcast_session(self, session_videos):
+        for video in session_videos:
+            #Announce the video in the session channel, if it is a paper presentation
+            if video.is_paper():
+                message = 'The video presentation for "' + video.paper.title + '" will be starting now!'
+                try:
+                    channel_name = Bot.get_valid_name(self.current_session_name, self.current_session_number)
+                    print('Sending presentation announcement to channel: ' + channel_name)
+                    await self.bot.send_message_by_name(channel_name, message) # Send message about the paper in the session channel
+                except Exception as ex:
+                    print('Error sending video announcement to session channel for paper ' + video.paper.title + 'Reason: ' + str(ex))
+            else:
+                print('Not a paper presentation. No announcement sent.')
             try:
-                if paper.presenter != None and isinstance(paper.presenter, str):
-                    message = message + '\nPresenter: ' + paper.presenter
-
-                await self.bot.send_message_by_name(paper.session, paper.session_number, message) # Send message about the paper in the session channel
-            except Exception as ex:
-                print('Error sending video announcement to session channel for paper ' + paper.name + 'Reason: ' + str(ex))
-            try:
-                self.player.play_video(paper.video_path)
+                print('Now playing: ' + video.video_path)
+                self.player.play_video(video.video_path)
                 time.sleep(1)
 
                 while True:
@@ -68,27 +89,52 @@ class CSCWManager:
                         break
                     continue
             except:
-                print('Error playing video ' + paper.video_path)
+                print('Error playing video ' + video.video_path)
 
             
 
-    # Sends message to the main session channel for the session and then broadcast it.
-    async def start_session (self, papers, filler_video = ''):
-        session_message = self.create_session_message(papers)
+    # Sends message to the main session channel for the session and then broadcast it. Reads the playlist and papers data from file
+    # at the beginning of each session, to allow for the user to change playlist or paper info, any time before the session starts.
+    async def start_session (self, session_number, session_name, filler_video = ''):
+        
+        playlist_data = pd.read_csv(self.playlist_file)
+        papers_data = pd.read_csv(self.papers_file)
+
+        session_videos = []
+
+        for i, playlist_video in playlist_data.iterrows():
+            if playlist_video["session_number"] == session_number:
+                paper = None
+                video_path = os.path.join(self.media_path, playlist_video["file_name"])
+
+                # If this is a paper, get the info for the paper
+                if playlist_video["is_paper"]:
+                    paper = Paper.get_paper(papers = papers_data, id = playlist_video["paper_id"], cycle = playlist_video["cycle"])
+                else:
+                    print('Not a paper')
+
+                session_videos.append(SessionVideo(session_number, video_path, paper))
+
+
+        session_message = self.create_session_message(session_videos, session_name)
 
         try:
             await self.bot.send_message(self.tv_channel_id, session_message)
         except Exception as ex:
-            print('Sending session message failed for session "' + papers[0].session + '" Reason: ' + str(ex))
+            print('Sending session message failed for session "' + str(session_number) + '. ' + str(session_name) +'" Reason: ' + str(ex))
 
         try:
-            await self.broadcast_session(papers)               
+            self.current_session_name = session_name
+            self.current_session_number = session_number
+            await self.broadcast_session(session_videos)               
         except:
-            print('Broadcasting session failed for session ' + papers[0].session)
+            print('Broadcasting session failed for session ' + str(session_number) + '. ' + str(session_name))
 
+        self.current_session_name = 'idle'
+        self.current_session_number = -1
         # Play a filler video after session, if specified.
         if filler_video != '':
-                try:
+                try:      
                     self.player.play_video(filler_video)
                 except:
                     print('Error playing the filler video') 
@@ -97,50 +143,52 @@ class CSCWManager:
     
 # Overall approach: Schedule all the video playlists to play for each session start time in both weeks and play the video(s) for each session in playlists. Ensure that all start times are in UTC and that the clock used is UTC. Iterate over each session row (indexed by #) in the time table and schedule the videos for each session in both weeks. Lookup the corresponding data for each session in session_data.
 async def main():
+    media_path = 'videos'
+    scheduling_path = 'scheduling'
+    playlist_file = os.path.join(scheduling_path, 'playlist.csv')
+    papers_file = os.path.join(scheduling_path, 'papers.csv')
+    sessions_file = os.path.join(scheduling_path, 'sessions.csv')
+    filler_video = os.path.join(media_path, 'cscw_filler.mp4')
+
     scheduler = AsyncIOScheduler(timezone=utc)
 
     load_dotenv()
     bot_token = os.getenv('TOKEN')
     live_tv_channel = int(os.getenv('TV_CHANNEL_ID'))
-    manager = CSCWManager(bot_token, live_tv_channel)
+    manager = CSCWManager(bot_token = bot_token, 
+        tv_channel_id = live_tv_channel,
+        playlist_file = playlist_file,
+        papers_file = papers_file,
+        media_path = media_path)
 
 
-    session_data = pd.read_csv(os.path.join(sessions_path, 'sessions.csv'))
-    timetable_data = pd.read_csv(os.path.join(sessions_path, 'timetable.csv'))
+    timetable_data = pd.read_csv(sessions_file)
 
-    print ('Scheduling videos...')
-    for i, session_times in timetable_data.iterrows():
-        session_papers = []
-        for m, paper_info in session_data.iterrows():
-            # Add all the paper ids with rows that have matching session ids to the lists for both weeks
-            if paper_info["session_number"] == session_times["session_number"]:
-                session_papers.append(PaperVideo(id = paper_info["cycle"] + '_' + str(paper_info["paper_id"]), 
-                title = paper_info["title"], 
-                session = paper_info["session_name"],
-                session_number = paper_info["session_number"],
-                presenter = paper_info["presenter"],
-                talk_number = int(paper_info["talk_number"]),
-                video_path=os.path.join(media_path, paper_info["cycle"] + '_' + str(paper_info["paper_id"]) + '.mp4')))
+    print ('Scheduling sessions...')
+    for i, session_row in timetable_data.iterrows():
         
     
         # Schedule the session to be broadcast at the correct time for both week 1 and week 2
-        w1_time = parser.parse(session_times["w1_time_utc"]).replace(tzinfo=timezone.utc)# Get the datetime for week 1 as utc
-        w2_time = parser.parse(session_times["w2_time_utc"]).replace(tzinfo=timezone.utc)# Get the datetime for week 2 as utc
-
-        filler_video = os.path.join(media_path, 'cscw_filler.mp4')
+        try:
+            w1_time = parser.parse(session_row["w1_time_utc"]).replace(tzinfo=timezone.utc)# Get the datetime for week 1 as utc
+            w2_time = parser.parse(session_row["w2_time_utc"]).replace(tzinfo=timezone.utc)# Get the datetime for week 2 as utc
+        except Exception as ex:
+            print('Could not parse date for row ' + str(i+2) + ' in file ' + sessions_file + '. Reason: ' + str(ex))
+            continue
 
         scheduler.add_job(manager.start_session, 'date', run_date=w1_time, kwargs = {
-            'papers': session_papers,
+            'session_number': session_row["session_number"],
+            'session_name': session_row["session_name"],
             'filler_video': filler_video
             })
         scheduler.add_job(manager.start_session, 'date', run_date=w2_time, kwargs = {
-            'papers': session_papers,
+            'session_number': session_row["session_number"],
+            'session_name': session_row["session_name"],
             'filler_video': filler_video
             })
 
     scheduler.start()
     print("Scheduling complete.")
-
     print('Starting Discord bot. Please keep this script running.')
 
     try:
