8fc6d231b966bd575878ada28f1c61651cb4b617
Oloff Biermann <51513555+ocbier@users.noreply.github.com>
2022-09-23 18:43:48 -0700

    Add downloader script

--- video_downloader/video_downloader.py
+++ video_downloader/video_downloader.py
@@ -1,21 +1,17 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Run "pip install gdown"
-
+# "pip install pandas"
 import gdown
+import pandas as pd
 
 
-def print_hi(name):
+def download(name):
     url = 'https://drive.google.com/open?id=1c0BIRg3qGyi7cZmqZfKuLWxMhzUX5Yl5'
     output = 'testdown.mp4'
     gdown.download(url, output, quiet=False, fuzzy=True)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+submissions = pd.read_csv('video_metadata')
+
