65b398446d2d4f6037e1c4fca412ce915b514450
Oloff Biermann <51513555+ocbier@users.noreply.github.com>
2022-09-25 04:54:28 -0700

    Resolve duplication issue with playlist generator

--- scripts/playlist_generator.py
+++ scripts/playlist_generator.py
@@ -4,6 +4,7 @@ import os
 
 media_path = os.path.join('..', 'videos')
 video_suffix = 'mp4'
+out_file = 'playlist.csv'
 
   
 
@@ -25,8 +26,7 @@ def map_cycle(cycle):
         case 'cscw22b':
             name = 'jan'
     
-    return cycle_name
-
+    return name
 
 def get_session_number(papers, cycle, paper_id):
 
@@ -44,17 +44,19 @@ papers_data = pd.read_csv(os.path.join('..', 'scheduling', 'papers.csv'))
 
 playlist_items = []
 
+print(submission_data.columns)
+
 for i, submission_info in submission_data.iterrows():
     pcs_url = submission_info["URL of your paper's PCS submission page"]
 
     # Ensure there is a PCS url
     if pcs_url is None:
-        print('No PCS URL provided for row ' + (i + 1))
+        print('No PCS URL provided for row ' + str(i + 1))
         continue
 
-    parts = pcs_url.split('/')
+    parts = str(pcs_url).split('/')
     if len(parts) < 7:
-        print('Invalid PCS URL for row ' + (i + 1))
+        print('Invalid PCS URL for row ' + str(i + 1))
         continue
 
     pcs_cycle = parts[3].strip()
@@ -62,14 +64,14 @@ for i, submission_info in submission_data.iterrows():
     video_file = pcs_cycle + '_' + paper_id + '.' + video_suffix
 
     # Ensure the video file exists
-    if not os.path.isfile(video_file):
-        print('The associated file for PCS URL ' + pcs_url + ' does not exist. ' + 'Expected file ' + video_file)
+    if not os.path.isfile(os.path.join(media_path, video_file)):
+        print('Associated file for PCS URL ' + pcs_url + ' not found. ' + 'Expected: ' + str(os.path.abspath(os.path.join(media_path, video_file))))
         continue
 
     cycle_name = map_cycle(pcs_cycle)
 
     if cycle_name is None:
-        print('Invalid cycle name for row ' + (i + 1))
+        print('Invalid cycle name for row ' + str(i + 1))
         continue
 
     session_number = get_session_number(papers_data, cycle_name, paper_id)
@@ -80,9 +82,11 @@ for i, submission_info in submission_data.iterrows():
     playlist_items.append(playlist_row)
     # End for
 
+# Create dataframe containing the playlist data
 out_df = pd.DataFrame(data=playlist_items, columns=['file_name', 'session_number', 'is_paper', 'paper_id', 'cycle', 'presenter'])
 
-    
+#Write to UTF-8 csv file
+out_df.to_csv(out_file, index=False, encoding='utf-8')
 
 
 
