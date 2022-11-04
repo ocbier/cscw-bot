0ea54c3e4bebbc84af7e4042c2c17d5715be7358
Oloff Biermann <4648384+DongwookYoon@users.noreply.github.com>
2022-11-04 13:58:37 -0700

    player stop

--- player.py
+++ player.py
@@ -10,18 +10,18 @@ class VLCPlayer:
         #if test_mode == False:
             #self.vlc_instance.log_unset()
 
-
     def play_video(self, video):
         media = self.vlc_instance.media_new(video)
         self.player.set_media(media)
         self.player.set_fullscreen(True)
         self.player.play()
 
-
     def is_playing(self):
         playing = set([1,2,3,4])
         return self.player.get_state() in playing
 
+    def stop(self):
+        return self.player.stop()
 
     def play_playlist(self, files):
         for video in files:
